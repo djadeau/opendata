c08ddc3d783a1269c1f4ed47c9512b9514f8e7a3
Damien JADEAU <jadeau.damien@outlook.fr>
2022-11-15 16:59:47 +0000

    Resolution des conflits après le merge de master

--- api/main.py
+++ api/main.py
@@ -56,9 +56,3 @@ def get_random_string(length):
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
-
-def get_random_string(length):
-    # choose from all lowercase letter
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
